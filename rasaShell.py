315a9a72bda87b1c7e5982e72044633f98548e34
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-08-29 18:21:27 +0200

    Update rasaShell.py

--- rasaShell.py
+++ rasaShell.py
@@ -9,7 +9,7 @@ nome = f.read()
 f.close()
 comando1 = "docker stop {}_app_1"
 comando2 = "docker stop {}_rasa_1"
-os.chdir("C:/Users/Antonio/Tirocinio/RasaApplication/" + cartella) # cambia la directory corrente
+os.chdir("C:/Users/Antonio/OneDrive/Documenti/GitHub/RasaApplicationTirocinio" + cartella) # cambia la directory corrente
 print(os.getcwd())
 subprocess.run("rasa shell", shell=True)
 subprocess.run(comando1.format(nome.lower()), shell=True)
