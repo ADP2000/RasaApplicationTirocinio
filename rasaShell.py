d379ae832fa08d960da526f85dbf1abb34d61a2a
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-08-31 15:52:11 +0200

    Update rasaShell.py

--- rasaShell.py
+++ rasaShell.py
@@ -4,13 +4,13 @@ import subprocess
 f = open("cartellaEsercizio.txt","r")
 cartella = f.read()
 f.close()
-f = open("nomeEsercizio.txt", "r")
-nome = f.read()
-f.close()
-comando1 = "docker stop {}_app_1"
-comando2 = "docker stop {}_rasa_1"
-os.chdir("C:/Users/Antonio/OneDrive/Documenti/GitHub/RasaApplicationTirocinio/" + cartella) # cambia la directory corrente
+# f = open("nomeEsercizio.txt", "r")
+# nome = f.read()
+# f.close()
+
+os.chdir(cartella) # cambia la directory corrente
 print(os.getcwd())
 subprocess.run("rasa shell", shell=True)
-subprocess.run(comando1.format(nome.lower()), shell=True)
-subprocess.run(comando2.format(nome.lower()), shell=True)
+# subprocess.run(comando1.format(nome.lower()), shell=True)
+# subprocess.run(comando2.format(nome.lower()), shell=True)
+subprocess.run("docker-compose stop", shell=True)
