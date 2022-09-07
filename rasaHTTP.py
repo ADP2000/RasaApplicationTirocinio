641faf610d85d66197f7e5bee1d86f727d8af7c8
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-07 15:22:51 +0200

    Create rasaHTTP.py

---
+++ rasaHTTP.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+f = open("cartellaEsercizio.txt","r")
+cartella = f.read()
+f.close()
+
+os.chdir(cartella) # cambia la directory corrente
+subprocess.run("python -m http.server", shell=True)
+subprocess.run("docker-compose stop", shell=True)
