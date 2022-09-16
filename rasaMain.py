acc961989e2f7b1b63bcd6d2e92cad3c116eccc5
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-16 12:52:15 +0200

    aggiunti moduli json e requests
    aggiunti i moduli di cui sopra per effettuare la selezione degli esercizi da svolgere per una specifica sessione e per restituire un feedback

--- rasaMain.py
+++ rasaMain.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import json
+from typing import List
+import requests
 from random import randint
 from eserciziA.eserciziA import lista_eserciziA
 from eserciziC.eserciziC import lista_eserciziC
@@ -26,35 +29,50 @@ levels = {
 for esercizio in lista_esercizi:
     levels[esercizio.livello].append(esercizio)
 
-def scegli_esercizio(livello) -> Esercizio:
-    lista = levels[livello]
-    i = randint(0,len(lista)-1)
-    return lista[i]
+endpoint = ""                            # deve essere modificato
+risposta = requests.get(endpoint)
+with open("sessione.json", "w") as outfile:
+    json.dump(risposta,outfile,indent=4)
 
-n = input("Scegli il livello di difficota da 1 a 3: ")
-numero = int(n)
-if(numero > 3):
-    print("Hai sbagliato numero. Deve essere compreso tra 1 e 3!")
-else:
-    esercizioScelto = scegli_esercizio(numero)
-    print(esercizioScelto.nome)
+sessionID = risposta.json()['id']
+esercizi = risposta.json()['exercises']  #lista degli esercizi da somministrare con id, nome e livello
 
+def getEsercizi(esercizi) -> List[Esercizio]:
+    nomiEsercizi = []
+    for e in esercizi:
+        nomiEsercizi.append(e['name'])
+
+    eserciziDaSvolgere = []
+    for ne in nomiEsercizi:
+        for esercizio in lista_esercizi:
+            if ne == esercizio.nome:
+                eserciziDaSvolgere.append(esercizio)
+    
+    return eserciziDaSvolgere
+
+eserciziScelti = getEsercizi(esercizi)
+f = open("feedback_{}.txt".format(sessionID), "w")
+f.write("SESSION ID = {}".format(sessionID))
+f.close()
+
+
+for esercizio in eserciziScelti:
     f = open("cartellaEsercizio.txt", "w")
-    f.write(esercizioScelto.cartella)
+    f.write(esercizio.cartella)
     f.close()
 
-    # f = open("nomeEsercizio.txt", "w")
-    # f.write(esercizioScelto.nome)
-    # f.close()
+    f = open("nomeEsercizio.txt", "w")
+    f.write(esercizio.nome)
+    f.close()
 
     cartellaCorrente = os.getcwd()
-    os.chdir(esercizioScelto.cartella)
+    os.chdir(esercizio.cartella)
     comando = "docker build . -t adp2000/prova_rasa:{}"
-    subprocess.run(comando.format(esercizioScelto.nome), shell=True)  # permette di costruire l'immagine dell'
-                                                                    # esercizio scelto ed eseguire il comando successivo
+    subprocess.run(comando.format(esercizio.nome), shell=True)  # permette di costruire l'immagine dell'
+                                                                        # esercizio scelto ed eseguire il comando successivo
     subprocess.run("docker-compose up -d", shell=True) # il comando docker-compose up esegue i container che si 
-                                                    # riferiscono ai server e il parametro -d li esegue in background 
+                                                        # riferiscono ai server e il parametro -d li esegue in background 
     os.chdir(cartellaCorrente)
     subprocess.run("py rasaShell.py", shell=True)  # esegue il file rasaShell.py che permette di eseguire l'esercizio
-                                                    # e si occupa della sucessiva deattivazione dei server
+                                                        # e si occupa della sucessiva deattivazione dei server
     #subprocess.run("py rasaHTTP.py", shell=True)    # esegue il file rasaHTTP.py
--- rasaShell.py
+++ rasaShell.py
@@ -1,14 +1,28 @@
 import os
 import subprocess
+import json
+
+
+data = json.load(open("sessione.json"))
 
 f = open("cartellaEsercizio.txt","r")
 cartella = f.read()
 f.close()
-# f = open("nomeEsercizio.txt", "r")
-# nome = f.read()
-# f.close()
+cartellaCorrente = os.getcwd()
 
 os.chdir(cartella) # cambia la directory corrente
-#print(os.getcwd())
 subprocess.run("rasa shell", shell=True)
+f = open("errori.txt", "r")
+errori = f.read()
+f.close()
+
 subprocess.run("docker-compose stop", shell=True)
+
+os.chdir(cartellaCorrente)
+f = open("nomeEsercizio.txt", "r")
+esercizio = f.read()
+f.close()
+
+f = open("feedback_{}.txt".format(data['id']),"a")
+f.write("\nE' stato svolto l'{} con numero di errori pari a {}".format(esercizio,errori))
+f.close()
