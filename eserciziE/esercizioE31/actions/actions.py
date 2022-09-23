75ab81fd3dc2d358fe17bc257adbab86d5431213
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-23 18:01:11 +0200

    update esercizioE31

---
+++ eserciziE/esercizioE31/actions/actions.py
@@ -0,0 +1,107 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+from typing import Any, Text, Dict, List
+import gtts
+import os
+from rasa_sdk.events import EventType, Restarted 
+from rasa_sdk import Action, Tracker , FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+storia = """Un giorno stavo tornando a casa da scuola, quando un cavallo, che era scappato con le redini sulla groppa, 
+      superò un gruppo di noi ed entrò nel campo di un contadino alla ricerca di un po’ di acqua da bere. 
+      Sudava abbondantemente, e il contadino non l’aveva visto, cosicché lo catturammo noi.
+      Io saltai in groppa al cavallo e, visto che aveva le briglie, presi in mano le redini e dissi:”Hop! Hop!”, 
+      indirizzandolo verso la strada. Sapevo che il cavallo avrebbe girato nella direzione giusta. 
+      E il cavallo si mise a trottare e a galoppare lungo la strada.
+      Ogni tanto si scordava di essere sulla strada e si buttava in qualche campo, 
+      allora io gli davo una scrollatina e richiamavo la sua attenzione sul fatto che era sulla strada che doveva stare.
+      E alla fine, a circa 4 miglia da dove gli ero salito in groppa, si infilò nel recinto di una fattoria e il contadino disse: 
+      ”Dunque è così che è tornato quello scemo. Ma dove l’hai trovato?”, e io dissi:” A circa 4 miglia da qui”.
+      “E come hai fatto a sapere che dovevi venire QUI?”. ”Io non lo sapevo”, risposi “Lo sapeva il cavallo. 
+      Io non ho fatto altro che mantenere la sua attenzione sulla strada”."""
+errori = -1
+
+class ActionRestart(Action):
+    def name(self) -> Text:
+        return "action_restart"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Digita di nuovo 'iniziamo' per giocare di nuovo")
+        return [Restarted()]
+
+class ActionNumeroCavalloAsk(Action):
+    def name(self) -> Text:
+        return "action_ask_numero_cavallo"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text = "\n\nQuante volte viene ripetuta la parola 'Cavallo'?\nRispondi nel seguente modo: 1,2,3,..."
+        )
+        return []
+
+class ActionStoria(Action):
+    def name(self) -> Text:
+        return "action_storia"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text= storia
+        )
+        tts = gtts.gTTS(storia, lang = "it")
+        tts.save("storia.mp3")
+        os.system("storia.mp3")
+        return []
+
+class ActionFine(Action):
+    def name(self) -> Text:
+        return "action_fine"
+
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        f = open("errori.txt","w")
+        f.write(str(errori))
+        f.close()
+        dispatcher.utter_message(
+            text = "Alla prossima volta",
+        )
+        return []
+
+class ValidatePlayForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_play_form"
+
+    def validate_numero_cavallo(
+        self,
+        slot_value: Any, 
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict ) -> Dict[Text,Any]: 
+        
+        global errori
+        if slot_value.lower() == "5":
+            if errori == -1:
+                errori = 0
+                dispatcher.utter_message(
+                    text= "Corretto, la risposta è giusta"
+                )
+                return {"numero_cavallo": slot_value, "numero_errori": errori}
+            else:
+                dispatcher.utter_message(
+                    text= "Corretto, la risposta è giusta"
+                )
+                return {"numero_cavallo": slot_value, "numero_errori": errori}    
+        else:
+            if errori == -1:
+                errori = 1
+                dispatcher.utter_message(text = "Purtroppo hai sbagliato.\nDai riprova")
+                return {"numero_cavallo": None}
+            else:
+                errori += 1
+                dispatcher.utter_message(text = "Purtroppo hai sbagliato.\nDai riprova")
+                return {"numero_cavallo": None}
