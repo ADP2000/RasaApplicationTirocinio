4fdd80389ea0c45776dd246da5eec2895eab61e2
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-23 17:11:16 +0200

    update esercizioE21

--- eserciziE/esercizioE21/actions/actions.py
+++ eserciziE/esercizioE21/actions/actions.py
@@ -6,12 +6,28 @@
 
 
 from typing import Any, Text, Dict, List
-
+import gtts
+import os
 from rasa_sdk.events import EventType, Restarted 
 from rasa_sdk import Action, Tracker , FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
+storia = """C'era una volta una gara.....di ranocchi.
+      L'obiettivo era arrivare in cima a una gran torre.
+      Si radunò molta gente per vedere e fare il tifo per loro.
+      Cominciò la gara.
+
+      In realtà, la gente probabilmente non credeva possibile che i
+      ranocchi raggiungessero la cima, e tutto quello che si ascoltava
+      erano frasi tipo: "Che pena !!! Non ce la faranno mai!"
+
+      I ranocchi cominciarono a desistere, tranne uno che continuava a  cercare di raggiungere la cima.  La gente continuava: 
+      "... Che pena !!! Non ce la faranno mai!..."
+      E i ranocchi si stavano dando per vinti tranne il solito ranocchio testardo che continuava ad insistere.
+      Alla fine, tutti desistettero tranne quel ranocchio che, solo e con  grande sforzo, raggiunse alla fine la cima.
+      Gli altri volevano sapere come avesse fatto. Uno degli altri ranocchi si avvicinò per chiedergli come avesse
+      fatto a concludere la prova. Fu così che scoprirono... che era sordo !"""
 errori = -1
 
 class ActionRestart(Action):
@@ -33,25 +49,17 @@ class ActionNumeroRanocchiAsk(Action):
         return []
 
 
-class ActionNumeroMaAsk(Action):
+class ActionStoria(Action):
     def name(self) -> Text:
-        return "action_ask_numero_altri"
+        return "action_storia"
 
     def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         dispatcher.utter_message(
-            text = "Quante volte viene ripetuta la parola 'altri'? \nRispondi nel seguente modo: 1 volta,2 volte,3 volte,..."
+            text= storia
         )
-        return []
-
-
-class ActionNumeroEspressioneAsk(Action):
-    def name(self) -> Text:
-        return "action_ask_numero_espressione"
-
-    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(
-            text = "Quante volte viene ripetuta la seguente espressione 'Che pena! Non ce la faranno mai'? \nRispondi nel seguente modo: 1 espressione,2 espressioni,3 espressioni,..."
-        )
+        tts = gtts.gTTS(storia, lang = "it")
+        tts.save("storia.mp3")
+        os.system("storia.mp3")
         return []
 
 class ActionFine(Action):
@@ -86,12 +94,12 @@ class ValidatePlayForm(FormValidationAction):
                 dispatcher.utter_message(
                     text= "Corretto, la risposta è giusta"
                 )
-                return {"numero_ranocchi": slot_value}
+                return {"numero_ranocchi": slot_value, "numero_errori": errori}
             else:
                 dispatcher.utter_message(
                     text= "Corretto, la risposta è giusta"
                 )
-                return {"numero_ranocchi": slot_value}
+                return {"numero_ranocchi": slot_value, "numero_errori": errori}
         else:
             if errori == -1:
                 errori = 1
@@ -102,43 +110,4 @@ class ValidatePlayForm(FormValidationAction):
                 dispatcher.utter_message(text = "Purtroppo hai sbagliato.\nDai riprova")
                 return {"numero_ranocchi": None}
 
-    def validate_numero_altri(
-        self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict ) -> Dict[Text,Any]: 
-
-        global errori
-        if slot_value.lower() == "2 volte":
-            dispatcher.utter_message(
-                text= "Corretto, la risposta è giusta"
-            )
-            return {"numero_altri": slot_value}
-        else:
-            errori += 1
-            dispatcher.utter_message(
-                text = "OPS! Hai sbagliato. Dai riprova"
-            )
-            return {"numero_altri": None}
-
-    def validate_numero_espressione(
-        self,
-        slot_value: Any, 
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict ) -> Dict[Text,Any]: 
-
-        global errori
-        erroriTot = errori
-        if slot_value.lower() == "2 espressioni":
-            dispatcher.utter_message(
-                text= "Corretto, la risposta è giusta"
-            )
-            return {"numero_espressione": slot_value, "numero_errori": erroriTot}
-        else:
-            dispatcher.utter_message(
-                text = "OPS! Hai sbagliato. Dai riprova"
-            )
-            errori += 1
-            return {"numero_espressione": None}
+    
