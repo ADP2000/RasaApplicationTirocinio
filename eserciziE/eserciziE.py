4a7e64a8cbed28c731c2f0b13702ec4e586ed5e1
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-23 18:31:23 +0200

    Update eserciziE.py

--- eserciziE/eserciziE.py
+++ eserciziE/eserciziE.py
@@ -8,11 +8,17 @@ class Esercizio:
         self.cartella = cartella
 
 
-esercizioE1 = Esercizio("esercizioE1",1, "eserciziE/esercizioE1")
-esercizioE2 = Esercizio("esercizioE2",2, "eserciziE/esercizioE2")
-esercizioE3 = Esercizio("esercizioE3",3, "eserciziE/esercizioE3")
-
-lista_eserciziE = [esercizioE1,esercizioE2,esercizioE3,]
+esercizioE11 = Esercizio("esercizioE11",1, "eserciziE/esercizioE11")
+esercizioE12 = Esercizio("esercizioE12",1, "eserciziE/esercizioE12")
+esercizioE13 = Esercizio("esercizioE13",1, "eserciziE/esercizioE13")
+esercizioE21 = Esercizio("esercizioE21",2, "eserciziE/esercizioE21")
+esercizioE22 = Esercizio("esercizioE22",2, "eserciziE/esercizioE22")
+esercizioE23 = Esercizio("esercizioE23",2, "eserciziE/esercizioE23")
+esercizioE31 = Esercizio("esercizioE31",3, "eserciziE/esercizioE31")
+esercizioE32 = Esercizio("esercizioE32",3, "eserciziE/esercizioE32")
+esercizioE33 = Esercizio("esercizioE33",3, "eserciziE/esercizioE33")
+lista_eserciziE = [esercizioE11, esercizioE12,esercizioE13,
+esercizioE21,esercizioE22, esercizioE23, esercizioE31, esercizioE32, esercizioE33]
 
 def getEserciziE() -> List:
     return lista_eserciziE
