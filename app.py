b42f15aeb4199cddc9cec06f09363b45ff8cbb17
ADP2000 <ant.dellaporta1@stud.uniroma3.it>
2022-09-28 11:22:31 +0200

    modificata sessione in app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ easySession = Session(id = "d290f1ee-6c54-4b01-90e6-d701748f0851", exercises = [
 hardSession = Session( id = "4c45382a-3cc2-11ed-a261-0242ac120002", exercises = [
     CognitiveExercise(id = "633d3afa-3cc2-11ed-a261-0242ac120002", name = "esercizioA3", level = 3),
     CognitiveExercise(id = "7d722c82-3cc2-11ed-a261-0242ac120002", name = "esercizioH1", level = 3),
-    CognitiveExercise(id = "9177aa04-3cc2-11ed-a261-0242ac120002", name = "esercizio E32", level = 3)
+    CognitiveExercise(id = "9177aa04-3cc2-11ed-a261-0242ac120002", name = "esercizioE32", level = 3)
 ])
 
 session = easySession
